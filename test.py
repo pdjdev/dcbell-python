b2143c860f47e0a94f671d63694a4d09c13e760d
Dongjun Park <pdj5096@gmail.com>
2019-03-24 22:47:22 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,48 +1,57 @@
-#크롤링이 잘 되는지 테스트하는 용도입니다.
-#아스키 테이블로 출력됩니다.
-
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from prettytable import PrettyTable
-import requests
 
-gallid = "euca" #갤러리 ID 입력
+gallid = input("갤러리 ID?: ")#갤러리 ID 입력
 
-print ("갤러리ID: " + gallid)
+html = urlopen("https://gall.dcinside.com/board/lists?id=" + gallid).read()
+
 print("요청시작...")
 
-html = urlopen('https://gall.dcinside.com/mgallery/board/lists?id=' + gallid).read()
-soup = BeautifulSoup(html, "html.parser")
-link = soup.find_all("tr", { "class" : "ub-content us-post"})
+try:
 
-print ("=========글 출력 시작=========")
+  print("갤러리 종류: ", end='')
+  if "location.replace" in str(html):
+    print("마이너 갤러리")
+    html = urlopen("https://gall.dcinside.com/mgallery/board/lists?id=" + gallid).read()
+  else:
+    print("정식 갤러리")
 
-tb = PrettyTable(['Num', 'Title', 'Name(IP)'])
-tb.align = 'l'
+  soup = BeautifulSoup(html, "html.parser")
+  link = soup.find_all("tr", { "class" : "ub-content us-post"})
 
-for m in link:
+  print ("=========글 출력 시작=========")
 
-  #게시글 제목
-  tmp = m.find("td", { "class" : "gall_tit ub-word"})
- 
-  if "<b>" not in str(tmp):
-    title = tmp.a.text
-      
-    #게시글 번호
-    postnum = m.find("td", { "class" : "gall_num"}).text
+  tb = PrettyTable(['Num', 'Title', 'Name(IP)'])
+  tb.align = 'l'
 
-    #게시글 작성자 (유동은 IP)
-    tmp = m.find("td", { "class" : "gall_writer ub-writer"})
-    name = tmp.find("em").text
-    ip = tmp.find("span", { "class" : "ip"})
+  for m in link:
 
-    if ip is not None:
-      ip = ip.text
-    else:
-      ip = "고닉"
+    #게시글 제목
+    tmp = m.find("td", { "class" : "gall_tit ub-word"})
+  
+    if "<b>" not in str(tmp):
+      title = tmp.a.text
+        
+      #게시글 번호
+      postnum = m.find("td", { "class" : "gall_num"}).text
 
-    tb.add_row([postnum, title, name + " (" + ip + ")" ])
+      #게시글 작성자 (유동은 IP)
+      tmp = m.find("td", { "class" : "gall_writer ub-writer"})
+      name = tmp.find("em").text
+      ip = tmp.find("span", { "class" : "ip"})
+
+      if ip is not None:
+        ip = ip.text
+      else:
+        ip = "고닉"
+
+      tb.add_row([postnum, title, name + " (" + ip + ")" ])
+  
+  print (tb)
+
+except Exception as ex:
+   print("오류 발생:", ex)
 
 
-print (tb)
 print ("===========작업 끝============")

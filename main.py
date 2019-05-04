292a589c3f746fe9cf0cd22fe87dd5d93abeec4a
Dongjun Park <pdj5096@gmail.com>
2019-05-04 21:41:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,10 @@ while(1):
 
   print("요청시작...")
 
-  html = urlopen('https://gall.dcinside.com/mgallery/board/lists?id=' + gallid).read()
+  hdr = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 'Accept-Encoding': 'none', 'Accept-Language': 'en-US,en;q=0.8', 'Connection': 'keep-alive'}
+  req = urllib.request.Request("https://gall.dcinside.com/mgallery/board/lists?id=" + gallid, headers = hdr)
+  html = urllib.request.urlopen(req).read()
+  
   soup = BeautifulSoup(html, "html.parser")
   link = soup.find_all("tr", { "class" : "ub-content us-post"})
 
